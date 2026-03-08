7853fba840a55cb5266999b5925ef9d59ac03120
Giulio Troccoli-Allard <giulio@troccoli.it>
2026-03-08 15:28:56 +0000

    feat: install Flux and Flux Pro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,10 @@
 
         <title>{{ $title ?? config('app.name') }}</title>
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @livewireStyles
@@ -14,5 +18,6 @@
         {{ $slot }}
 
         @livewireScripts
+        @fluxScripts
     </body>
 </html>
